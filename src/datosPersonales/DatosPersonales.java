1fd6585fb217ae885cdf098cbf73b26fab87387b
Legach <lautarito-hernan.14@live.com.ar>
2023-02-01 12:51:41 -0300

    agregando metodo Main para imprimir en consola datos personales

---
+++ src/datosPersonales/DatosPersonales.java
@@ -0,0 +1,19 @@
+package datosPersonales;
+
+public class DatosPersonales {
+	
+	public static void main(String[] args) {
+		
+		String nombre = "Lautaro";
+		String apellido = "Rojas";
+		int edad = 23;
+		String lugarDeResidencia = "Villa Urquiza";
+		
+		System.out.println("Su nombre es: " + nombre);
+		System.out.println("Su apellido es: " + apellido);
+		System.out.println("Tiene " + edad + " años");
+		System.out.println("Y vive en: " + lugarDeResidencia);
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lauta
+ *
+ */
+module datospersonales {
+}
